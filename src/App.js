61cc0b2c5cb3130ca01fd5e0ab0d12d04b5c3d9f
mdcohen1993 <mdcohen1993@users.noreply.github.com>
2020-11-29 07:20:13 +0200

    ms4-wip-UPDATED

--- src/App.js
+++ src/App.js
@@ -86,11 +86,10 @@ class App extends React.Component {
 
 
   render() {
-    let subtitle;
     const newDate = new Date();
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     const modals = this.state.notes;
-    const modalItems = modals.map(modal =>
+    modals.map(modal =>
     {
         return (
         <div className='list' key={modal.key} onClick={this.handleOpenModal}>
